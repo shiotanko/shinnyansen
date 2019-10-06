d2991f245655d7f183caacf64052bd94a2777409
山内志織 <shiori252pon@gmail.com>
2019-10-06 18:46:33 +0900

    トップページ　jqueryの追加（20191007）

--- js/top.js
+++ js/top.js
@@ -8,7 +8,49 @@ $(function(){
         else{ //scroll up
             activePoint.removeClass('active');
         }
-    },{offset : '50%'});
-  }); 
-  
+    },{offset : '70%'});
+
+
+
+  $('.top-backnumber__scroll').waypoint(function(direction){
+    var activePoint = $(this.element);
+    if (direction === 'down') {//scroll down
+        activePoint.addClass('active');
+    }
+    else{ //scroll up
+        activePoint.removeClass('active');
+    }
+    },{offset : '90%'});
+
+    $('.top-member__wrap2').waypoint(function(direction){
+        var activePoint = $(this.element);
+        if (direction === 'down') {//scroll down
+            activePoint.addClass('active');
+        }
+        else{ //scroll up
+            activePoint.removeClass('active');
+        }
+        },{offset : '80%'});
+    
+        $('.top-performance-wrap2').waypoint(function(direction){
+            var activePoint = $(this.element);
+            if (direction === 'down') {//scroll down
+                activePoint.addClass('active');
+            }
+            else{ //scroll up
+                activePoint.removeClass('active');
+            }
+            },{offset : '80%'});
+       
+        $('.recrute-coment').waypoint(function(direction){
+            var activePoint = $(this.element);
+            if (direction === 'down') {//scroll down
+                activePoint.addClass('active');
+            }
+            else{ //scroll up
+                activePoint.removeClass('active');
+            }
+            },{offset : '80%'});
+
+  });   
  
